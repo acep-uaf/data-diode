3156c726613b100e0bc57647ff78ae15a4e67d4b
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2024-02-11 18:07:01 -0900

    refactor: MQTT republisher example to stream

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 	"time"
 
 	"github.com/acep-uaf/data-diode/utility"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/urfave/cli/v2"
 	"gopkg.in/yaml.v2"
 )
@@ -178,56 +177,6 @@ func sampleMetrics(server string, port int) {
 	fmt.Println(">> Value: ", utility.Value())
 }
 
-func demoRepublisher(server string, port int, topic string, message string) {
-	fmt.Println(">> MQTT")
-	fmt.Println(">> Broker: ", server)
-	fmt.Println(">> Port: ", port)
-
-	// Source: https://github.com/eclipse/paho.mqtt.golang/blob/master/cmd/simple/main.go
-	var example mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-		fmt.Printf(">> Topic: %s\n", msg.Topic())
-		fmt.Printf(">> Message: %s\n", msg.Payload())
-	}
-
-	mqtt.DEBUG = log.New(os.Stdout, "", 0)
-	mqtt.ERROR = log.New(os.Stdout, "", 0)
-
-	// Initial Connection
-	opts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s:%d", server, port))
-	opts.SetKeepAlive(2 * time.Second)
-	opts.SetDefaultPublishHandler(example)
-	opts.SetPingTimeout(1 * time.Second)
-
-	// Create and start a client using the above ClientOptions
-	client := mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-
-	// Subscribe to a topic
-	if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-
-	// Publish to a topic
-	token := client.Publish(topic, 0, false, message)
-	token.Wait()
-
-	time.Sleep(6 * time.Second)
-
-	// Disconnect from the broker
-	if token := client.Unsubscribe(topic); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-
-	client.Disconnect(250)
-
-	time.Sleep(1 * time.Second)
-
-}
-
 func main() {
 	data, err := os.ReadFile("config.yaml")
 
@@ -317,7 +266,7 @@ func main() {
 				Usage:   "MQTT (TCP stream) demo",
 				Action: func(mCtx *cli.Context) error {
 					fmt.Println("----- MQTT -----")
-					demoRepublisher(mqttBrokerIP, mqttBrokerPort, mqttBrokerTopic, mqttBrokerMessage)
+					utility.Republisher(mqttBrokerIP, mqttBrokerPort, mqttBrokerTopic, mqttBrokerMessage)
 					return nil
 				},
 			},
