16d57a0323259ab2f98202bfbe207599197d8442
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2024-03-14 14:48:09 -0800

    docs: mock external API for schema validation

--- diode_test.go
+++ diode_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	insights "github.com/acep-uaf/data-diode/insights"
@@ -14,8 +15,22 @@ var (
 	FileChecksum        = "477076c6fd8cf48ff2d0159b22bada27588c6fa84918d1c4fc20cd9ddd291dbd"
 )
 
+type TCP struct {
+	ClientTargetIP      string
+	ClientTargetPort    int
+	ServerTargetIP      string
+	ServerPort          int
+	ServerSocketTimeout int
+}
+
 func TestAPI(t *testing.T) {
 	jsonFile, err := os.Open(BackupConfiguration)
+
+	schema := "CAMIO.2024.1.0"
+	version := filepath.Base(jsonFile.Name())
+
+	// FIXME: Cross reference the JSON contents, schema version, & configuration file?
+	fmt.Println(version, schema)
 
 	if err != nil {
 		t.Errorf("[?] %s via %s", err, jsonFile.Name())
