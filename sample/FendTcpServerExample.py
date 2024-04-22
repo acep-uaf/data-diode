6e4554121a7124ec8ba5ba4e5a1f6ca513de1517
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2024-04-21 16:20:44 -0800

    docs: Python comment for non-MIT licensed code

--- sample/FendTcpServerExample.py
+++ sample/FendTcpServerExample.py
@@ -1,4 +1,7 @@
-""" 
+# Fend TCP and UDP Client/Server Example Python Test Scripts
+# Copyright (c) 2023 Fend Incorporated. All rights reserved.
+
+"""
 This is an example Python script provided to aid in the integration of a Fend Data Diode in a TCP Client/Server application.
 
 For this example a simple socket is opened and listens for the diode's TCP client to connect. When data is received it is printed to
@@ -7,7 +10,7 @@ the console window.
 For these scripts to function, the diode will need to be setup in TCP client/server mode. Please see the user manual
 on instruction for this setup process.
 
-These scripts will continuously run until aborted. 
+These scripts will continuously run until aborted.
 
 The following is the expected step by step process of what the script does:
 1. The script creates a socket object named "server".
@@ -18,35 +21,35 @@ The following is the expected step by step process of what the script does:
 """
 
 import socket
- 
+
 # Change this to the Target TCP Server IP Address in your diode's Output Side TCP Client Settings.
 targetTcpServerIP = "192.168.1.20"
 
 # Change this to the Target TCP Server Port in your diode's Output Side TCP Client Settings.
-targetTcpServerPort = 503 
+targetTcpServerPort = 503
 
 # Set up a TCP server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((targetTcpServerIP, targetTcpServerPort))
- 
+
 # Begin listening
 server.listen(1)
- 
+
 while True:
     print("Waiting for connection")
     connection, client = server.accept()
- 
+
     try:
         # Print to console the connected client IP address
         print("Connected to client IP: {}".format(client))
-         
+
         # Receive and print data 10 Kbytes at a time, as long as the client is sending something
         while True:
             data = connection.recv(10240)
             print(f"Received data: {data.decode()}")
- 
+
             if not data:
                 break
- 
+
     finally:
         connection.close()
